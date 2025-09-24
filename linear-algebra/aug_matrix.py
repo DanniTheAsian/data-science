83e19fdc15c9340e43815f322a69f8ca788b4f3c
Thien Ly Buu Huynh <dummy.xiu@gmail.com>
2025-09-24 17:38:43 +0200

    implemented user input

--- linear-algebra/aug_matrix.py
+++ linear-algebra/aug_matrix.py
@@ -1,35 +1,62 @@
 # import af libraries
 import numpy as np 
-from sympy import Matrix
+from sympy import Matrix, pprint
+import sys
 
 
-#En numpy array med flere lister
-Augmented_Matrix = np.array([
-    [1,1,0,3], #R1 x1 + x2
-    [0,1,-1,2], #R2 x2 + x3
-    [2,1,1,4] #R3 x1 + x2 + x3
-    #tilføj flere lister, hvis nødvendigt
-])
-#Konverter matrix 
-matrix = Matrix(Augmented_Matrix)
+def main():
 
-#Row echelon form
-ref_matrix = matrix.echelon_form()
-print("===REF (row echelon form)===")
-print (ref_matrix)
+    print("=== Augumented Matrix ===\n")
 
-#Reduced echelon form
-rref_matrix, pivots = matrix.rref()
-print("\n=== RREF (Reduced row echelon form)===")
-print(rref_matrix)
+    # prompting the user
+    rows = int(input("Enter number of rows: m = "))
+    cols = int(input("Enter number of columns: n = "))
 
-#tager alle rækker og den sidste kolonne
-solution = list(rref_matrix[:, -1])
+    # asking the user to enter the elements of a matrix
+    print("\n=== Enter the elementes row by row: ===\n")
+    augmented_matrix = creating_matrix(rows, cols)
+    print("\n===The matrix you typed is: ===\n\n", augmented_matrix)
 
-#lav navne: x1, x2, x3.......x100
-variables = [f"x{i+1}" for i in range(len(solution))]
+    # Konverterer en NumPy array (matrix) til en SymPy Matrix
+    matrix = Matrix(augmented_matrix)
 
-print("\n===Solution ===")
-#var = navnet på variablerne, zip = parrer elementerne sammen
-for var, value in zip(variables, solution):
-    print(f"{var} = {float(value):.2}")
+    # Row echelon form (REF)
+    ref_matrix = matrix.echelon_form()
+    print("\n===REF (row echelon form)===\n")
+    pprint(ref_matrix)
+
+    # Reduced row echelon form (RREF)
+    rref_matrix, pivots = matrix.rref()
+    print("\n=== RREF (Reduced row echelon form) ===\n")
+    pprint(rref_matrix)
+
+
+    # tager alle rækker og den sidste kolonne
+    solution = list(rref_matrix[:, -1])
+
+    #lav navne: x1, x2, x3.......x100
+    variables = [f"x{i+1}" for i in range(len(solution))]
+
+    print("\n===Solution ===\n")
+    #var = navnet på variablerne, zip = parrer elementerne sammen
+    for var, value in zip(variables, solution):
+        print(f"{var} = {float(value):.2}")
+
+
+def creating_matrix(rows: int, cols: int):
+    """ Generates a matrix based on user input """
+
+    # initializing a NumPy Matrix that consists of 0's with float types.
+    matrix_zeros = np.zeros((rows,cols), dtype=float)
+
+    # replacing 0's with user-input elements
+    for i in range(rows):
+        for j in range(cols):
+            matrix_zeros[i, j] = int(input(f"Enter element at position ({i+1},{j+1}): "))
+
+    # Print the matrix
+    return matrix_zeros
+
+
+if __name__ == "__main__":
+    main()
