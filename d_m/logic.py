eee445e0951b2bd8ca08775ad4815bb0ed115a91
Thien Ly Buu Huynh <dummy.xiu@gmail.com>
2025-10-13 12:17:44 +0200

    disjunction

--- d_m/logic.py
+++ d_m/logic.py
@@ -1,1 +1,14 @@
 
+
+def main():
+	print("p    q    r")
+	for p in [True, False]:
+		for q in [True, False]:
+			a = disjunction(p,q)
+			print(p,q,a)
+
+def disjunction(p, q):
+	return p or q
+
+if __name__=="__main__":
+	main()
